45315581bd0c38601c5694d6f781ac2cb3754edd
Abderrahman Hmaymou <ahmaymou@e2r3p3.1337.ma>
2023-03-05 15:17:34 +0100

    parsing ...

---
+++ main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahmaymou <ahmaymou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/04 12:24:53 by ahmaymou          #+#    #+#             */
+/*   Updated: 2023/03/05 15:17:05 by ahmaymou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "minishell.h"
+
+int main()
+{
+	// (void)ac;
+	// (void)av;
+	// struct sigaction	sa;
+
+	prompt();
+	prompt();
+	// sa.sa_sigaction = handle_kill;
+	// sigaction(SIGINT, &sa, NULL);
+	// while (1);
+}
---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahmaymou <ahmaymou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/04 12:18:23 by ahmaymou          #+#    #+#             */
+/*   Updated: 2023/03/05 15:16:46 by ahmaymou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdbool.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <stdio.h>
+# include "libft/libft.h"
+
+void    prompt();
